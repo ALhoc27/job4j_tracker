011680f040420ee8cf3abb3593b89f7a0e61e9be
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-26 15:18:51 +0500

    .

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat(); /* Здесь мы создаем переменную типа Cat с именем peppy. Далее мы записываем в эту переменную ссылку на объект типа Cat.*/
+        /*Важный момент. Оператор new резервирует кусок памяти и записывает в нее данные объекта класса Cat.*/
+        Cat sparky = new Cat();
+    }
+}

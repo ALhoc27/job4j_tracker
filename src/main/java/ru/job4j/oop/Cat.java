8309cb7c6bcd04cd2f63331e1f14b706741ca76f
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-27 08:48:25 +0500

    .

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,16 +2,44 @@ package ru.job4j.oop;
 
 public class Cat {
 
-        public String sound() {
-            String voice = "may-may";
-            return voice;
-        }
+    private String food;
+    private String name;
 
-    public static void main(String[] args) {
+    public void show() {
+        System.out.println("nikname = '" + name + "'");
+        System.out.println(name + " was eating a " + this.food + "\n");
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
+    void giveNick(String nick) {
+        name = nick;
+    }
+
+    public static void main(
+            String[] args) {
         Cat peppy = new Cat(); /* Здесь мы создаем переменную типа Cat с именем peppy. Далее мы записываем в эту переменную ссылку на объект типа Cat.*/
         /*Важный момент. Оператор new резервирует кусок памяти и записывает в нее данные объекта класса Cat.*/
         Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black");
+        black.show();
     }
 }

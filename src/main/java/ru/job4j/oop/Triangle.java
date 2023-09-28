fcf14ce805e33b00813a1e7d1252ce57c6d6e48b
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-28 14:55:37 +0500

    .

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -1,14 +1,20 @@
 package ru.job4j.oop;
 
 public class Triangle {
-    private final double firstDistSecond;
-    private final double secondDistThird;
-    private final double firstDistThird;
+    /*  private final double firstDistSecond;
+      private final double secondDistThird;
+      private final double firstDistThird;*/
+    private Point first;
+    private Point second;
+    private Point third;
 
     public Triangle(Point ap, Point bp, Point cp) {
-        firstDistSecond = ap.distance(bp);
+    /*  firstDistSecond = ap.distance(bp);
         firstDistThird = ap.distance(cp);
-        secondDistThird = bp.distance(cp);
+        secondDistThird = bp.distance(cp);*/
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
     }
 
     public double semiPerimeter(double ab, double bc, double ac) {
@@ -21,11 +27,21 @@ public class Triangle {
 
     public double area() {
         double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
 
-        if (this.exist(firstDistSecond, firstDistThird, secondDistThird)) {
-            double p = semiPerimeter(firstDistSecond, firstDistThird, secondDistThird);
-            rsl =  Math.sqrt(p * (p - firstDistSecond) * (p - secondDistThird) * (p - firstDistThird));
+        if (this.exist(ab, ac, bc)) {
+            double p = semiPerimeter(ab, ac, bc);
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
         }
         return rsl;
+
+/*        if (this.exist(firstDistSecond, firstDistThird, secondDistThird)) {
+            double p = semiPerimeter(firstDistSecond, firstDistThird, secondDistThird);
+            rsl = Math.sqrt(p * (p - firstDistSecond) * (p - secondDistThird) * (p - firstDistThird));
+        }
+        return rsl;*/
+
     }
 }

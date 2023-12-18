9439699e43062d894448d56daf9cd79754a3e2bf
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-12-18 16:14:11 +0500

    .

--- src/main/java/ru/job4j/pojo/Book.java
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -30,22 +30,4 @@ public class Book {
         this.count = count;
     }
 
-    public void printBook(Book a) {
-        System.out.println("Название книги: " + a.getBookTitle() + "\n" + "Кол-во страниц: " + a.getCount());
-    }
-
-    static public void printBook(Book[] a) {
-        System.out.println("Массив книг: " + a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("№ " + i + " Книга: " + a[i].getBookTitle());
-        }
-    }
-
-    static public Book[] rearrangementOf2ElementsArrayBook(Book[] a, int x, int y) {
-        Book bookTemp = a[x];
-        a[x] = a[y];
-        a[y] = bookTemp;
-        return a;
-    }
-
 }
--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -7,9 +7,9 @@ public class Library {
         Book cleanCode = new Book("Clean code", 65);
         Book boston = new Book("Boston", 114);
         Book[] books = {lake, woeFromWit, cleanCode, boston};
-        Book.printBook(books);
-        Book.rearrangementOf2ElementsArrayBook(books, 0, 3);
-        Book.printBook(books);
+        printBook(books);
+        rearrangementOf2ElementsArrayBook(books, 0, 3);
+        printBook(books);
         printBookWithFilter(books, "поиск 'Clean code'");
     }
 
@@ -20,6 +20,24 @@ public class Library {
                 System.out.println("№ " + i + " Книга: " + a[i].getBookTitle());
             }
         }
+    }
+
+    public void printBook(Book a) {
+        System.out.println("Название книги: " + a.getBookTitle() + "\n" + "Кол-во страниц: " + a.getCount());
+    }
+
+    static public void printBook(Book[] a) {
+        System.out.println("Массив книг: " + a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("№ " + i + " Книга: " + a[i].getBookTitle());
+        }
+    }
+
+    static public Book[] rearrangementOf2ElementsArrayBook(Book[] a, int x, int y) {
+        Book bookTemp = a[x];
+        a[x] = a[y];
+        a[y] = bookTemp;
+        return a;
     }
 
 }

cd40b365fa44024b53ee37e805e50514b73c99e7
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-12-18 07:21:38 +0500

    .

--- src/main/java/ru/job4j/pojo/Book.java
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -1,5 +1,7 @@
 package ru.job4j.pojo;
 
+import java.util.Objects;
+
 public class Book {
     private String bookTitle;
     private int count;
@@ -26,6 +28,24 @@ public class Book {
 
     public void setCount(int count) {
         this.count = count;
+    }
+
+    public void printBook(Book a) {
+        System.out.println("Название книги: " + a.getBookTitle() + "\n" + "Кол-во страниц: " + a.getCount());
+    }
+
+    static public void printBook(Book[] a) {
+        System.out.println("Массив книг: " + a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("№ " + i + " Книга: " + a[i].getBookTitle());
+        }
+    }
+
+    static public Book[] rearrangementOf2ElementsArrayBook(Book[] a, int x, int y) {
+        Book bookTemp = a[x];
+        a[x] = a[y];
+        a[y] = bookTemp;
+        return a;
     }
 
 }
--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -7,13 +7,20 @@ public class Library {
         Book cleanCode = new Book("Clean code", 65);
         Book boston = new Book("Boston", 114);
         Book[] books = {lake, woeFromWit, cleanCode, boston};
-
+        Book.printBook(books);
+        Book.rearrangementOf2ElementsArrayBook(books, 0, 3);
+        Book.printBook(books);
+        printBookWithFilter(books, "поиск 'Clean code'");
     }
 
-    public void printBook(Book[] a) {
-        for (int i = 0; i <= a.length; i++) {
-            System.out.println(a[i]);
+    static public void printBookWithFilter(Book[] a, String nameFilterField) {
+        System.out.println("Массив книг (c фильтром по " + nameFilterField + "): " + a);
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].getBookTitle().equals("Clean code")) {
+                System.out.println("№ " + i + " Книга: " + a[i].getBookTitle());
+            }
         }
     }
+
 }
 

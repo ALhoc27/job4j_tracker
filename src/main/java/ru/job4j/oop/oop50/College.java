c2607250e2e9b875f7b0d60679fce5b56e64fb0f
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-12-29 00:06:22 +0500

    .

---
+++ src/main/java/ru/job4j/oop/oop50/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop.oop50;
+
+import java.util.Objects;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Object object = freshman;
+        Student student = freshman;
+    }
+}
---
+++ src/main/java/ru/job4j/oop/oop50/Student.java
@@ -0,0 +1,4 @@
+package ru.job4j.oop.oop50;
+
+public class Student {
+}

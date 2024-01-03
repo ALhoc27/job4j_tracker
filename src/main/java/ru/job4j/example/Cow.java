3f5ee383a5388f803afac5ee3eb2294d4067604f
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-01-03 19:30:25 +0500

    .

--- src/main/java/ru/job4j/example/Cow.java
+++ src/main/java/ru/job4j/example/Cow.java
@@ -4,4 +4,9 @@ public class Cow extends DomesticAnimal {
     public void giveMilk() {
         System.out.println(nameClass + " дает человеку молоко.");
     }
+
+    public void sayCow() {
+        System.out.println("Say-Say");
+    }
+
 }

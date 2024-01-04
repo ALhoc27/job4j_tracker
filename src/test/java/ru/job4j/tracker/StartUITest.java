03162deae97b1c090839aa85c149e09c5919035d
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-01-04 14:13:38 +0500

    .

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,47 @@ class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    void whenAddItem1() {
+        String[] answers = {"Fix PC", "Fix Bug"};
+        Input input = new MockInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+        created = tracker.findAll()[1];
+        expected = new Item("Fix Bug");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "edited item"
+        };
+        StartUI.replaceItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void whenDellItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+            String.valueOf(item.getId()),
+        };
+        StartUI.deleteItem(new MockInput(answers), tracker);
+        int id = Integer.parseInt(answers[0]);
+        Item edited = tracker.findById(item.getId());
+        assertNull(edited);
+    }
 }

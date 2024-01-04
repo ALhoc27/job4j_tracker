14e8bc5104065e0f0999afafcb1bbb2799488c7f
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-01-04 18:59:43 +0500

    .

---
+++ src/main/java/ru/job4j/tracker/FindByIdAction.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class FindByIdAction implements UserAction {
+    @Override
+    public String name() {
+        return "Найти заявку по id";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        int id = input.askInt("Введите id заявки (которую хотим получить): ");
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.println("Заявка с введенным id: " + id + " не найдена.");
+        }
+        return true;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+public class ReplaceAction implements UserAction {
+    @Override
+    public String name() {
+        return "Изменить заявку";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        int id = input.askInt("Введите id заявки(которую меняем): ");
+        String name = input.askStr("Введите имя новой заявки: ");
+        boolean result = tracker.replace(id, new Item(name));
+        if (result) {
+            System.out.println("Заявка изменена успешно.");
+        } else {
+            System.out.println("Заявка не изменена");
+        }
+        return true;
+    }
+}

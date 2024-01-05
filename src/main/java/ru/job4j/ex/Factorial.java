574a10e52ff88c5f517edb6befc83c2311aebb72
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-01-05 18:18:42 +0500

    .

---
+++ src/main/java/ru/job4j/ex/Factorial.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+public class Factorial {
+    public int calc(int number) {
+        this.exeptionLegArg(number);
+        int result = 1;
+        for (int index = 2; index <= number; index++) {
+            result *= index;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Factorial().calc(2));
+    }
+
+    private void exeptionLegArg(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number could not be less than 0");
+        }
+    }
+}

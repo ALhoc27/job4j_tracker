895a2c5af75a7d615ced75160c88dcc9309a9e1c
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-01-06 22:58:46 +0500

    .

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,26 +1,30 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int result = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                result = i;
-                break;
-            }
-        }
-        if (result == -1) {
-            throw new ElementNotFoundException("Нет элемента");
-        }
+        /* for-each */
         return result;
     }
 
-    public static void main(String[] args) {
-        try {
-            FindEl.indexOf(new String[]{"1e1e2", "1ffeff", "sasa", "sasaa"}, "sasaaa");
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        /* if contains throw ElementAbuseException */
+        return true;
+    }
 
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
     }
 }

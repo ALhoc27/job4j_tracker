652ea6b7f3b96466826d1d5c85bdf3e6651edf48
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-01-06 16:57:24 +0500

    .

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,8 +6,7 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 result = i;
-                System.out.println("Элемент найден: " + (i + 1));
-                return i;
+                break;
             }
         }
         if (result == -1) {

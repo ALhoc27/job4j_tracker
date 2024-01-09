cc1fc34ba45d562602e28c53ba6b3f3d143ad276
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-01-09 23:59:19 +0500

    .

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,8 +23,8 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        final int COUNT_ENGINE = getName().equals("A380") ? 4 : 2;
-        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+        int count = "A380".equals(getName()) ? 4 : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + count);
     }
 
     @Override

8e29727be3f19102829a350f2249826102e817b6
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2024-02-02 09:13:54 +0500

    ]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -7,20 +7,13 @@ import java.util.Objects;
 public class UsageArrayList {
     public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
-        ArrayList arrayList1 = new ArrayList();
         arrayList.add("Petr");
         arrayList.add("Ivan");
         arrayList.add("Stepan");
-        arrayList1.add("Petr");
-        arrayList1.add("Ivan");
-        arrayList1.add("Stepan");
 
         for (String index : arrayList) {
             System.out.println("index = " + index);
         }
 
-        for (Object index : arrayList1) {
-            System.out.println("(String) index = " + (String) index);
-        }
     }
 }
